ef17b452325d079357d6f751b8fd33e4946071ff
KogarkoP <kogarkopavel@gmail.com>
2025-12-17 00:54:51 +0200

    Acomplished task 5

--- 5_Santas_Magical_ASCII_Countdown/src/App.js
+++ 5_Santas_Magical_ASCII_Countdown/src/App.js
@@ -1,15 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
-import digits from "./assets/data/digitsArray";
+import { DIGITS } from "./assets/data/digitsArray";
 
 const App = () => {
   const [time, setTime] = useState(10);
+  const [number, setNumber] = useState([]);
+  const [message, setMessage] = useState("");
+  const messages = {
+    10: "🎅✨ Santa is preparing… ✨🎅",
+    7: "🎅✨ Reindeer are buckling up… ✨🎅",
+    5: "🎅✨ Sleigh warming up... ✨🎅",
+    3: "🎅✨ Almost ready... ✨🎅",
+    0: "🎅✨ SANTA’S SLEIGH IS LAUNCHING!!!!! ✨🎅",
+  };
 
-  const countdown = () => {};
+  const combineDigits = (digits) => {
+    return digits[0].map((_, rowIndex) =>
+      digits.map((digit) => digit[rowIndex]).join("  ")
+    );
+  };
+
+  const getDigitColor = (time) => {
+    if (time > 7) return "green";
+    if (time > 3) return "gold";
+    return "red";
+  };
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setTime((prevTime) => {
+        if (prevTime === 0) return 0;
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(countdown);
+  }, []);
+
+  useEffect(() => {
+    if (time === 10) {
+      const combined = combineDigits([DIGITS[1], DIGITS[0]]);
+      setNumber(combined);
+    } else {
+      setNumber(DIGITS[time]);
+    }
+
+    if (messages[time]) setMessage(messages[time]);
+  }, [time]);
 
   return (
     <div className={styles.wrapper}>
-      <button>Start the Countdown</button>
+      <pre className={styles.digit} style={{ color: getDigitColor(time) }}>
+        {number.join("\n")}
+      </pre>
+      <p className={styles.message}>{message}</p>
     </div>
   );
 };
