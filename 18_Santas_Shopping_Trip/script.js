54b9568e751d9c11e6fec2c85b0a3e0b26ce1831
KogarkoP <kogarkopavel@gmail.com>
2026-01-03 00:11:06 +0200

    Acomplished task 18

---
+++ 18_Santas_Shopping_Trip/script.js
@@ -0,0 +1,29 @@
+const boughtItemsInStores = [
+  [1.07, 2.92, 3.45, 1.09, 0.89],
+  [1.08, 2.35, 3.75, 1.12, 0.69],
+  [0.98, 2.48, 3.62, 1.1, 0.72],
+];
+
+const calculateSantaExpenses = (stores) => {
+  let totalSpent = 0;
+
+  for (let i = 0; i < stores.length; i++) {
+    let storeTotal = 0;
+    for (let j = 0; j < stores[i].length; j++) {
+      storeTotal += stores[i][j];
+    }
+
+    console.log(
+      `In store ${i + 1}, ${
+        stores[i].length
+      } items were bought and Santa spent ${storeTotal.toFixed(2)}€`
+    );
+    totalSpent += storeTotal;
+  }
+
+  console.log(
+    `Santa totally spent ${totalSpent.toFixed(2)}€ in ${stores.length} stores`
+  );
+};
+
+calculateSantaExpenses(boughtItemsInStores);
