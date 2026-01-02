b70a451c94a36e263ecb6288e48ea81e75ec8844
KogarkoP <kogarkopavel@gmail.com>
2026-01-02 23:08:30 +0200

    Fixed task 17

--- 17_Santas_Magic_Juice_Bottling_Challenge/script.js
+++ 17_Santas_Magic_Juice_Bottling_Challenge/script.js
@@ -1,17 +1,19 @@
-const juiceAmounts = [45, 92, 33];
+const elves = [
+  { name: "Sparkle", liters: 45 },
+  { name: "Twinkle", liters: 92 },
+  { name: "Jingle", liters: 33 },
+];
 
-const results = juiceAmounts.map((litersOfJuice) => {
-  const amountOf5lBarrels = Math.floor(litersOfJuice / 5);
-  const leftoverAfter5l = litersOfJuice % 5;
+elves.forEach((elf) => {
+  const amountOf5lBarrels = Math.floor(elf.liters / 5);
+  const leftoverAfter5l = elf.liters % 5;
 
   const amountOf2lJugs = Math.floor(leftoverAfter5l / 2);
   const leftoverAfter2l = leftoverAfter5l % 2;
 
   const amountOf1lFlasks = leftoverAfter2l;
 
-  return [amountOf5lBarrels, amountOf2lJugs, amountOf1lFlasks];
+  console.log(
+    `${elf.name}: ${amountOf5lBarrels} ${amountOf2lJugs} ${amountOf1lFlasks}`
+  );
 });
-
-for (let i = 0; i < results.length; i++) {
-  console.log(results[i].join(" "));
-}
